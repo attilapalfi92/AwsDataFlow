c747d9322786375eab352650d00c372e996d0570
attila.palfi <attila.palfi@evosoft.com>
2017-08-04 13:18:24 +0200

    initial commit

---
+++ src/main/kotlin/com/attilapalfi/awsdataflow/AwsDataFlow/AwsDataFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.attilapalfi.awsdataflow.AwsDataFlow
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AwsDataFlowApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AwsDataFlowApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/attilapalfi/awsdataflow/AwsDataFlow/AwsDataFlowApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.attilapalfi.awsdataflow.AwsDataFlow
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AwsDataFlowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
