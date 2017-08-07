42320f08d261c8ddfefdfb07fb29feb90e3a66b9
attilapalfi92 <attila.palfi.92@gmail.com>
2017-08-07 08:34:22 +0200

    added working lambda app and cloudformation

--- LambdaApps/EnvVarTest.py
+++ LambdaApps/EnvVarTest.py
@@ -1,4 +1,12 @@
+import boto3
+import json
 import os
 
+
 def lambda_handler(event, context):
-    return os.environ('TEST_ENV')
+    envValue = os.environ['TEST_ENV']
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='TestQueue1')
+    print envValue
+    queue.send_message(MessageBody=json.dumps(event))
+    return envValue
